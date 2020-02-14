eb0b4ac1917a93aab1b5fffae98de2458671cf46
Romain Michel <romain.michel@engie.com>
2020-02-14 10:28:00 +0100

    add command line parameter to allow specifying log file

--- src/coindre_optim/__main__.py
+++ src/coindre_optim/__main__.py
@@ -3,20 +3,22 @@ import logging
 import argparse
 
 if __name__ == "__main__":
-    # setup logging
-    logging.basicConfig(
-        format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
-        datefmt="%y-%m-%d %H:%M:%S",
-        handlers=[logging.FileHandler("coindre_optim.log"), logging.StreamHandler()],
-        level=logging.INFO,
-    )
-
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument("--help", action="help")
     parser.add_argument("--config_file", dest="config_file", nargs="?", default=None,
                         required=True,
                         help="Path to a yaml config file")
+    parser.add_argument("--log_file", dest="log_file", nargs="?", default="coindre_optim.log",
+                        required=False,
+                        help="Path the file to be used for logging")
     args = parser.parse_args()
+
+    logging.basicConfig(
+        format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+        datefmt="%y-%m-%d %H:%M:%S",
+        handlers=[logging.FileHandler(args.log_file), logging.StreamHandler()],
+        level=logging.INFO,
+    )
 
     runner = coindre_optim.runner.Runner(config_path=args.config_file)
     runner.launch()
